fbc31af4712831b137252fad430d618146180861
Asir Intisar <aintisar48@gmail.com>
2025-02-14 16:24:08 +0600

    use nevigate and create error page

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,18 +1,26 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Post = ({post}) => {
     const {id,title} = post
+
+    const navigate = useNavigate()
+
     const postStyle ={
         margin:'10px',
         border: '2px solid red',
         borderRadius: '10px',
         padding: '10px',
     }
+    
+    const handleShowDetails = () =>{
+        navigate(`/posts/${id}`)
+    }
     return (
         <div style={postStyle}>
             <h3>ID: {id}</h3>
             <p>{title}</p>
-            <button><Link to={`/posts/${id}`}>Post Details</Link></button>      
+            <button><Link to={`/posts/${id}`}>Show Details</Link></button>      
+            <button onClick={handleShowDetails}>Click to see details</button>      
         </div>
     );
 };
